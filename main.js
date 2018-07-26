0addc22b8e33e323b3081d1694380a969739c144
Phallzin <41746365+Phallzin@users.noreply.github.com>
2018-07-26 13:38:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1310, height: 795})
+  mainWindow = new BrowserWindow({width: 1320, height:800})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
